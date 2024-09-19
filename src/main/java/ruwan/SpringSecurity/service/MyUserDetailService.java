62b355edf24799c1507bfb45798e8a2b5a24b79c
ruwan <liyanage.ruwan27@gmail.com>
2024-09-19 15:45:10 +0900

    unwanted remove spring security dependency

--- src/main/java/ruwan/SpringSecurity/service/MyUserDetailService.java
+++ src/main/java/ruwan/SpringSecurity/service/MyUserDetailService.java
@@ -1,6 +1,5 @@
 package ruwan.SpringSecurity.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
